f87f6d6539c3dc93c6c35a99a14caae3f2e8c228
Samuel Joseph <josephsamuel034@gmail.com>
2024-04-05 01:22:28 +0100

    chg

--- screens/UsersScreen.tsx
+++ screens/UsersScreen.tsx
@@ -1,6 +1,21 @@
 import { Text } from "react-native";
+import axios from "axios";
+import { useEffect } from "react";
 
 const UsersScreen = () => {
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        console.log("Response:", response.data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+  }, []);
+
   return <Text>Users Screen</Text>;
 };
 
